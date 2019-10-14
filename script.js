dceebbbdaa156e1a734187b82e2891c928c65c5b
miodrag.vukovic <vukovic.development@gmail.com>
2019-10-14 14:04:29 +0200

    Template building
    Completely random choice of answers, and correct answer implemeted ( i hate hardcoding )

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
+// GLOBAL VARIABLES
 
 let numOfQuestions = 3;
 let numArr = [];
+let correctAnswer;
+// IF THE API WAS WELL GROUPED, THESE WOULD BE OBSOLETE...
+let numberOfContinents = 7;
+let numberOfImages = 5;
+let answers = document.querySelector('.answers');
+
+// RANDOM NUMBER FUNCTION 
 
 function randomize(param) {
-	return Math.floor(Math.random() * (param + 1));
+	return Math.floor(Math.random() * param);
 }
 
-function generateQuestions() {
+
+// ARRAY OF RADOM NUMBERS FUNCTION
+
+function generateUniqueNumbers(max) {
 	while ( numArr.length < numOfQuestions ) {
-	    var num = Math.floor(Math.random() * (10 + 1));
-	    if ( numArr.indexOf(num ) === -1 )
+	    var num = Math.floor(Math.random() * max);
+	    if ( numArr.indexOf(num) === -1 )
 	    	numArr.push(num);
 	}
 }
 
-// generateQuestions()
+// MAIN API REQUEST
 
 const request = new XMLHttpRequest();
 var url = "https://api.myjson.com/bins/a6da9";
@@ -22,10 +33,28 @@ request.open('get', url, true);
 request.onreadystatechange = function() {
 	if ( request.readyState == 4 && request.status == 200 ) {
 		let data = JSON.parse(request.responseText);
-		generateQuestions();
-		for ( let i = 0; i < numArr.length; i++ ) {
-			console.log(data[numArr[i]]);
+		let string = "";
+		generateUniqueNumbers(numberOfContinents);
+		let arrIndex = (numArr.map(x => x * numberOfImages)).reverse();
+
+		// RANDOM CORRECT ANSWER CHOICE
+
+		correctAnswer = randomize(numOfQuestions);
+		console.log(correctAnswer)
+		let correctAnswerImage = arrIndex[correctAnswer] + randomize(numberOfImages);
+		document.querySelector('.image').setAttribute('src', data[correctAnswerImage].image)
+
+		console.log(correctAnswerImage)
+
+		// 3 RANDOM CONTINENTS CHOICE FOR ANSWERS FIELD
+
+		console.log(arrIndex)
+		for ( let i = 0; i < arrIndex.length; i++ ) {
+			let el = data[arrIndex[i]];
+			string += "<li>" + el.continent + "</li>";
+
 		}
+		answers.innerHTML = string;
 	}
 }
 request.send()
